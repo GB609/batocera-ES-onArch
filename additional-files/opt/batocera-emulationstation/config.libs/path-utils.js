8a86e229f1d6d097d6df06f0ef1659ad7e5ba148
GB609 <39741460+GB609@users.noreply.github.com>
2025-02-04 10:00:45 +0000

    [build] generate *.cfg files for batocera from drop-in dirs during makepkg
    - introduce pacman hook 90-create-batocera-es-config.hook:
      Used to merge package configs with user-added when updating
    - /bin/emulationstation: configure rom paths on start up
    - config-import.js: also expose BTC_BIN_DIR

--- additional-files/opt/batocera-emulationstation/config.libs/path-utils.js
+++ additional-files/opt/batocera-emulationstation/config.libs/path-utils.js
@@ -1,7 +1,8 @@
 
 const { isAbsolute, resolve, extname, basename, relative } = require('node:path');
 const { existsSync } = require('node:fs');
-const SYSTEMS_GREP = `grep -Eho '<system>|<path>.*</path>|<name>.*</name>' ${getConfigHome()}/es_systems*.cfg`;
+const USER_SYSTEM_CONFIGS = `"${getConfigHome()}"/es_systems*.cfg`
+const SYSTEMS_GREP = "grep -Eho '<system>|<path>.*</path>|<name>.*</name>'";
 const VALID_ROM_PATH = /^(\w[\w\S ]+\/)?([\w\S ]+?)\/?$/;
 const ROMS_DIR_TAG = '%ROMSDIR%';
 
@@ -12,34 +13,29 @@ function romInfoFromPath(romPath, system = null) {
   }
   romPath = _sanitizeSysCfgPath(romPath);
 
-  let sysPathMappings = {};
-  let systemPath;
-  let { execSync } = require('node:child_process');
-  let systems = execSync(SYSTEMS_GREP, { encoding: 'utf8' }).split('<system>');
-  systems.forEach(_ => Object.assign(sysPathMappings, _xmlToSysMapping(_)));
+  let sysPathMappings = readSystemRomPaths(USER_SYSTEM_CONFIGS);
 
+  let systemPath;
   if (system == null) {
     [system, systemPath] = Object.entries(sysPathMappings).find(entry => romPath.startsWith(entry[1])) || [null, null];
+  } else {
+    systemPath = sysPathMappings[system] || null;  
   }
 
-  systemPath = sysPathMappings[system] || null;
-  if (systemPath != null) { relativeRomPath = relative(systemPath, romPath) }
+  if (systemPath != null && romPath.startsWith(systemPath)) { relativeRomPath = relative(systemPath, romPath) }
 
   //there is no configured system path for the given rom, but a system was given as argument
   //treat the rom as being at the root path for the sake of property path analysis
   if (relativeRomPath == null && system != null) {
-    if (typeof systemPath == "undefined" || systemPath == null) {
+    if (systemPath == null) {
       throw new Error(`[${romPath}] outside of known system paths and given system [${system}] is not valid.`);
     }
-    let fakedRootRom = romInfoFromPath(resolve(systemPath, basename(romPath)));
-    fakedRootRom.absPath = romPath;
-    return fakedRootRom;
+    let fakedRomPath = resolve(systemPath, basename(romPath));
+    return Object.assign(romInfoFromPath(fakedRomPath), {absPath : romPath});
   }
 
   let result = VALID_ROM_PATH.exec(relativeRomPath);
-  if (result == null) {
-    throw new Error(`[${relativeRomPath}] does not match valid path spec [${VALID_ROM_PATH}]`);
-  }
+  if (result == null) { throw new Error(`[${relativeRomPath}] does not match valid path spec [${VALID_ROM_PATH}]`) }
 
   let extension = extname(result[2]).substring(1);
   return {
@@ -59,28 +55,48 @@ const PATH_TAG = /<.?path>/g;
 const TILDE_START = /^~\//;
 /** 
  * expects to output of the grep above.
- * <name>, and <path> will always come in pairs
+ * <name>, and <path> must come in pairs
  */
 function _xmlToSysMapping(sysString) {
   let [val1, val2] = sysString.trim().split('\n');
   if ([val1, val2].includes(undefined)) { return {} }
-  if (NAME_TAG.test(val1)) {
-    return { [val1.replace(NAME_TAG, '')]: val2.replace(PATH_TAG, '') }
-  }
-  return { [val2.replace(NAME_TAG, '')]: val1.replace(PATH_TAG, '') }
+  let name, path;
+  if (NAME_TAG.test(val1) && PATH_TAG.test(val2)) { name = val1; path = val2; } 
+  else if (NAME_TAG.test(val2) && PATH_TAG.test(val1)) { name= val2; path = val1; }
+  else { return {} } //invalid system, got <name> or <path> 2 times in a row
+  return { [name.replace(NAME_TAG, '').trim()]: path.replace(PATH_TAG, '').trim() }
 }
+
 function _sanitizeSysCfgPath(sysPath) {
   return resolve(sysPath.replace(TILDE_START, getHome()).replace(ROMS_DIR_TAG, getRomDir()));
 }
 
-function getHome() { return process.env['ES_HOME'] || process.env['HOME']; }
-function getConfigHome() { return process.env["ES_CONFIG_HOME"] || (getHome() + "/.emulationstation") }
+function getHome() { return resolve(process.env['ES_HOME'] || process.env['HOME']) }
+function setHome(newDir = getHome()) { process.env["ES_HOME"] = newDir }
+
+function getConfigHome() { return resolve(process.env["ES_CONFIG_HOME"] || (getHome() + "/.emulationstation")) }
+function setConfigHome(newDir = getConfigHome()) { process.env["ES_CONFIG_HOME"] = newDir }
+
 function getRomDir() { return resolve(process.env['ROMS_ROOT_DIR'] || '~/ROMs') }
+function setRomDir(newDir = getRomDir()) { process.env['ROMS_ROOT_DIR'] = newDir }
+
+function readSystemRomPaths(...fileGlobs){
+  if(fileGlobs.length == 0) { fileGlobs.push(USER_SYSTEM_CONFIGS) }
+  let { execSync } = require('node:child_process');
+  let { XML } = require('./parsing.js').XML;
+  let fullSource = XML.removeComments(execSync(`/bin/cat ${fileGlobs.join(' ')}`, {encoding: 'utf8'})).join('\n');
+  let systems = execSync(`${SYSTEMS_GREP}}`, { encoding: 'utf8', input: fullSource }).split('<system>');
+  
+  let sysPathMappings = {};
+  systems.forEach(_ => Object.assign(sysPathMappings, _xmlToSysMapping(_)));
+  return sysPathMappings;
+}
 
 module.exports = {
-  romInfoFromPath,
-  getHome,
-  getConfigHome,
-  getRomDir,
-  ROMS_DIR_TAG
+  romInfoFromPath, readSystemRomPaths, 
+  resolveRomPath: _sanitizeSysCfgPath,
+  getHome, setHome,
+  getConfigHome, setConfigHome, 
+  getRomDir, setRomDir,
+  ROMS_DIR_TAG, USER_SYSTEM_CONFIGS
 }

84e360f411e512b69fd7102129fa196df0997b92
GB609 <39741460+GB609@users.noreply.github.com>
2025-03-31 08:39:23 +0000

    Add 2 simple config parser tests

---
+++ test/js/libs/parsing.test.js
@@ -0,0 +1,41 @@
+import * as assert from 'node:assert/strict';
+
+let parser = require("./config.libs/parsing")
+
+class ParserTests {
+  parserImplicitConfStyle(){
+    let source = `
+      global.ui = false
+      global.another = 42
+      # some *.conf-style line comment
+      system.core.default = "string with blank"
+    `
+
+    let result parser.parseDict(source);
+    assert.deepEqual(result, {
+      global: {
+        ui: false,
+        another: 42
+      },
+      system: { core: {default:"string with blank"}}
+    })
+  }
+  
+  parseLineCommentedJsonString(){
+    let source = `{
+      "firstKey": "somestring",
+      //"commented": true,
+      "booleanValue": true,
+      "some_number": 42609
+    }`
+
+    let result = parser.jsonToDict(source);
+    assert.deepEqual(result, {
+      firstKey: "somestring",
+      booleanValue: true
+      some_number: 42609
+    });
+  }
+}
+
+runTestClass(ParserTests)

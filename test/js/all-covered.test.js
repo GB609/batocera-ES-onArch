85c9c540dcaff6825ac7eb0ae84cce169d039444
GB609 <39741460+GB609@users.noreply.github.com>
2025-04-02 11:00:35 +0200

    include btc-config in coverage

--- test/js/all-covered.test.js
+++ test/js/all-covered.test.js
@@ -1,9 +1,14 @@
 const { resolve } = require('path');
+const { symlinkSync } = require('node:fs');
 const { execSync } = require('node:child_process');
 
 // require each file once to get it added to coverage
 
 let searchPath = resolve(`${SRC_PATH}`);
+
+try { symlinkSync(searchPath + '/btc-config', searchPath + '/cfg.js'); }
+catch (e) {}
+
 console.log("Searching source files for coverage in path:", searchPath);
 let found = execSync(`find '${searchPath}' -name '*.js'`, { encoding: 'utf8' });
 found.trim().split('\n').forEach(file => {

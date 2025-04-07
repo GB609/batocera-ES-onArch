e6c027d6d3cc93d87545e14fbe61e35e129feb5b
GB609 <39741460+GB609@users.noreply.github.com>
2025-04-07 11:28:55 +0000

    [test] introduce tests for data-utils
    Also directly fix a bug in data-utils discovered by the test: Allow empty subobjects

---
+++ test/js/libs/data-utils.test.js
@@ -0,0 +1,111 @@
+const assert = require('node:assert/strict');
+
+const utils = requireSrc('./config.libs/data-utils.js');
+
+const testData = {
+  firstObj: {
+    firstObjSub1: true,
+    firstObjSub2: 42,
+    firstObjSub3: {
+      evenDeeper: []
+    },
+    'with blank' : false
+  },
+  secondArr: [2, 3, 4, 5, 6],
+  thirdScalar: "string",
+  'top-level blank': 88
+}
+
+function nameGenerator(testDict, testFun, key){ return `key:${key}` }
+
+function assertHierarchicKeyValue(key, testObject, expected){
+  let hkey = utils.HierarchicKey.from(key);
+  let actual = hkey.get(testObject);
+  assert.deepEqual(actual, expected, `${hkey.toString()} returned wrong value: ${actual} != ${expected}`);
+}
+
+class HierarchicKeyTests {
+  static testGetWithArrayKeys = parameterized(
+    [
+      [ [], testData, testData ],
+      [ ['firstObj'], testData, testData.firstObj ],
+      [ ['firstObj', 'firstObjSub1'], testData, true ],
+      [ ['firstObj', 'firstObjSub2'], testData, 42 ],
+      [ ['firstObj', 'firstObjSub3'], testData, {evenDeeper:[]} ],
+      [ ['firstObj', 'with blank'], testData, false ],
+      [ ['secondArr'], testData, testData.secondArr ],
+      [ ['secondArr', 2 ], testData, 4 ],
+      [ ['top-level blank'], testData, 88 ],
+    ],
+    assertHierarchicKeyValue,
+    nameGenerator
+  )
+
+  static testGetWithStringKeys = parameterized(
+    [
+      [ '', testData, testData ],
+      [ 'firstObj', testData, testData.firstObj ],
+      [ 'firstObj.firstObjSub1', testData, true ],
+      [ 'firstObj.firstObjSub2', testData, 42 ],
+      [ 'firstObj.firstObjSub3', testData, {evenDeeper:[]} ],
+      [ 'firstObj."with blank"', testData, false ],
+      [ 'secondArr', testData, testData.secondArr ],
+      [ 'secondArr.2', testData, 4 ],
+      [ '"top-level blank"', testData, 88 ],
+    ],
+    assertHierarchicKeyValue,
+    nameGenerator
+  )
+}
+
+class DataTransformationTests {
+  beforeEach(){ this.testData = sanitizeDataObject(testData) }
+
+  deepImplode(){
+    const expected = {
+      'firstObj.firstObjSub1': true,
+      'firstObj.firstObjSub2': 42, 
+      'firstObj.firstObjSub3.evenDeeper': [],
+      'firstObj["with blank"]': false,
+      'secondArr[0]': 2,
+      'secondArr[1]': 3,
+      'secondArr[2]': 4,
+      'secondArr[3]': 5,
+      'secondArr[4]': 6,
+      'thirdScalar': 'string',
+      '["top-level blank"]': 88
+    }
+    assert.deepEqual(utils.deepImplode(this.testData), expected);
+  }
+
+  removeEmpty(){
+    const expected = {
+      firstObj: {
+        firstObjSub1: true,
+        firstObjSub2: 42,
+        'with blank' : false
+      },
+      secondArr: [2, 3, 4, 5, 6],
+      thirdScalar: "string",
+      'top-level blank': 88
+    }
+    assert.deepEqual(utils.removeEmpty(this.testData), expected);
+  }
+
+  diffObjects(){
+    let base = utils.removeEmpty(sanitizeDataObject(this.testData));
+    this.testData.firstObj.firstObjSub3.evenDeeper.push("abc");
+    let result = utils.diff(base, this.testData);
+    assert.deepEqual(result, {
+      firstObj: {
+        firstObjSub3: {evenDeeper: [ 'abc' ]}
+      }
+    })
+  }
+}
+
+runTestClasses(
+  'Data Utilty Tests',
+  HierarchicKeyTests,
+  DataTransformationTests
+);

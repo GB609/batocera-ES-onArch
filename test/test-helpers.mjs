f9dfac07d056bccb835f05c77b04bd2ec05e35cd
GB609 <39741460+GB609@users.noreply.github.com>
2025-04-08 10:18:12 +0200

    [test] improve capabilities of default parameterized name generator

--- test/test-helpers.mjs
+++ test/test-helpers.mjs
@@ -1,18 +1,23 @@
 import { suite, test, before, after, beforeEach, afterEach } from 'node:test';
 
-function type(something){
-  if(Array.isArray(something)){
+function type(something) {
+  if (Array.isArray(something)) {
     return "array"
+  } else if (something instanceof RegExp) {
+    return "regex"
   }
   return typeof something;
 }
 const TYPE_STRINGIFIER = {
-  function: (fn)=>fn.name,
-  array: (arr)=>arr.map(stringify).join(', '),
-  object: JSON.stringify,
-  string: str => `"${str}"`
+  function: (fn) => fn.name,
+  regex: (r) => r.toString(),
+  array: (arr) => JSON.stringify(arr, stringify).replace(/(?<!\\)"/g, '').replace(/\\"/g, '"'),
+  object: (obj) => JSON.stringify(obj, stringify).replace(/(?<!\\)"/g, '').replace(/\\"/g, '"'),
+  string: str => `'${str}'`
 }
-function stringify(something){
+function stringify(something, value) {
+  if (something == "") { return value }
+  if (typeof value != "undefined") { something = value }
   let valType = type(something);
   let stringifier = TYPE_STRINGIFIER[valType] || String
   return stringifier(something);
@@ -20,7 +25,9 @@ function stringify(something){
 
 function defaultParameterizedNameBuilder(testDict, testFunction, ...parameters) {
   let index = Object.entries(testDict).length;
-  return `${testFunction.name}[${index}] - [${stringify(parameters)}]`;
+  if (parameters.length == 1) { parameters = parameters.pop() }
+  let stringified = `${stringify(parameters)}`;
+  return `${testFunction.name}[${index}] - ${stringified}`;
 }
 
 export function parameterized(parameterList, testFunction, nameBuilder = defaultParameterizedNameBuilder) {
@@ -30,7 +37,37 @@ export function parameterized(parameterList, testFunction, nameBuilder = default
     if (Array.isArray(constellation)) { params = [...constellation] }
     else { params = [constellation] }
 
-    let name = nameBuilder(testDict, testFunction, ...params)
+    let nameParams = [];
+    let nameBuilderFunction = nameBuilder;
+    switch(type(nameBuilder)){
+      case "array":
+        for (idx of nameBuilder) {
+          nameParams.push(params[idx] || undefined)
+        }
+        break;
+      case 'object':
+        let customized = {}
+        for (let [key, value] of Object.entries(nameBuilder)) {
+          customized[key] = params[value];
+        }
+        nameParams.push(customized);
+        break;
+      case 'string':
+        let resultingName = nameBuilder;
+        params.forEach((value, index) => {
+          let keyToReplace = new RegExp(`\\$\\{${index}\\}`, 'g');
+          let replacement = stringify(null, value);
+          resultingName = resultingName.replace(keyToReplace, replacement);
+        })
+        nameBuilderFunction = () => resultingName
+        break;
+      default:
+        nameParams.push(...params)
+    }
+
+    if (typeof nameBuilderFunction != "function") { nameBuilderFunction = defaultParameterizedNameBuilder }
+
+    let name = nameBuilderFunction(testDict, testFunction, ...nameParams)
     testDict[name] = function() { testFunction(...params) };
   }
 
@@ -100,33 +137,32 @@ export function runTestClass(testClass) {
   });
 }
 
-function CallingModuleName(){
+function CallingModuleName() {
   Error.captureStackTrace(this);
   this.nonameCounter = 0;
-  this.toString = function(){ return this.getCallingModuleName() };
+  this.toString = function() { return this.getCallingModuleName() };
 
-  this.getCallingModuleName = function(){
+  this.getCallingModuleName = function() {
     let lines = this.stack.split('\n');
-    for(let l of lines){
-      if(/Error|.*\/test-helpers.mjs\:.*/.test(l)) { continue }
+    for (let l of lines) {
+      if (/Error|.*\/test-helpers.mjs\:.*/.test(l)) { continue }
       let match = /at .* \((.*js)\:\d+\:\d+\)/.exec(l);
-      if(match){ return match[1]; }
+      if (match) { return match[1]; }
     }
     return __filename + `_${this.nonameCounter++}`;
   }
 }
 
-export function runTestClasses(name, ...classes){
-  if(typeof name != "string"){
+export function runTestClasses(name, ...classes) {
+  if (typeof name != "string") {
     classes = [name, ...classes];
     name = new CallingModuleName().toString();
   }
 
-  console.log("run test classes:", classes)
   suite(name, () => {
     classes.forEach(runTestClass);
   });
 }
 
 /** some general assertion helper */
-export function sanitizeDataObject(props){ return JSON.parse(JSON.stringify(props.valueOf())) }
+export function sanitizeDataObject(props) { return JSON.parse(JSON.stringify(props.valueOf())) }

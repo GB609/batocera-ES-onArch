f992cb20a546451e04d4d26ce493b213b5659d31
GB609 <39741460+GB609@users.noreply.github.com>
2025-04-10 12:41:25 +0000

    Create update-pages.sh

---
+++ scripts/update-pages.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Creates a sub-directory under the directory root/pages/versions that is named after $1, if '--push' is supplied as $2, the local changes are commited to remote
+# if no "$1" the current git branch name is expected to match 'pages/somever' and everything following the first / is taken
+# The created directory will have the contents of root/tmp/docs, so make sure this has run before
+
+source $(dirname $(realpath -s "$0"))/paths.sh
+
+if [ -z "$1" ] || [ "$1" = "--push" ]; then
+  branchName=$(git rev-parse --abbrev-ref HEAD || exit 1)
+  version="${branchName#pages\/}"
+else
+  version="$1"
+  shift
+fi
+
+if [ -z "$version" ]; then
+  echo "version is empty - cancel action"
+  exit 1
+fi
+
+DOC_TARGET="$ROOT_DIR"/pages/versions/"$version"
+
+if [ "$1" = "--push" ]; then
+  PUBLISH=true
+fi
+
+git checkout pages
+git pull --rebase origin main
+
+mkdir -p $(dirname "$DOC_TARGET")
+rm -rf "$DOC_TARGET"
+mv "$ROOT_DIR"/tmp/docs "$DOC_TARGET"
+
+git add .
+git commit -m "'(re)publish docs for ${version}'"
+
+git push origin pages --dry-run

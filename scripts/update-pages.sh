78761930a0998a26614361ce7b42710ae8a2c62c
GB609 <39741460+GB609@users.noreply.github.com>
2025-04-10 10:44:03 +0200

    [build] introduce pages generator for online documentation
    1. Github pages is fed from branch `pages`, contents in directory `pages`.
    2. Branches named `page-update/versionName` build sub-pages with `scripts/create-docs.js`, creating output for the current code in `tmp/docs` (tmp is ignored by git)
    3. Then `pages` is checked out and rebased on `master`
    4. `tmp/docs` is moved to `pages/versions/versionName`
    5. `pages` branch is force-pushed

---
+++ scripts/update-pages.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Creates a sub-directory under the directory root/pages/versions that is named after $1, if '--push' is supplied as $2, the local changes are commited to remote
+# if no "$1" the current git branch name is expected to match 'pages/somever' and everything following the first / is taken
+# The created directory will have the contents of root/tmp/docs, so make sure this has run before
+
+source $(dirname $(realpath -s "$0"))/paths.sh
+
+if [ -z "$1" ] || [ "$1" = "--push" ]; then
+  branchName=$(git rev-parse --abbrev-ref HEAD || exit 1)
+  version="${branchName#pages\/}"
+else
+  version="$1"
+  shift
+fi
+
+if [ -z "$version" ]; then
+  echo "version is empty - cancel action"
+  exit 1
+fi
+
+DOC_TARGET="$ROOT_DIR"/pages/versions/"$version"
+
+if [ "$1" != "--push" ]; then
+  PUBLISH="--dry-run"
+fi
+
+echo -e "\n::group::get remote branches"
+(
+  set -x
+  git fetch --all -f -p
+  git branch --remote
+) || exit 1
+echo '::endgroup::'
+
+echo -e "\n::group::get & update 'pages'"
+(
+  set -x
+  git switch pages
+  git pull --rebase -X ours -s ort origin main || (
+    echo "Rebase failed!\nWorkspace differences are:"
+    git diff
+    git status
+    exit 1
+  )
+) || exit 1
+echo '::endgroup::'
+
+echo -e "\n::group::update pages source directory"
+(
+  set -x
+  mkdir -p $(dirname "$DOC_TARGET")
+  rm -rf "$DOC_TARGET"
+  mv "$ROOT_DIR"/tmp/docs "$DOC_TARGET"
+) || exit 1
+echo '::endgroup::'
+
+echo -e "\n::group::Publish updates 'pages' branch"
+(
+  set -x
+  git add .
+  git status
+  git commit -m "'(re)publish docs for ${version}'"
+  
+  git push $PUBLISH origin pages:pages
+) || exit 1
+echo '::endgroup::'

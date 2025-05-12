fe948d3875539499d125e5908bf656dd1f8131a7
GB609 <39741460+GB609@users.noreply.github.com>
2025-05-12 08:34:44 +0200

    enhance syntax checking to also include all kinds of bash scripts

--- scripts/check-syntax.sh
+++ scripts/check-syntax.sh
@@ -15,14 +15,35 @@ fi
 SRC_DIRS=(
   "$SRC_DIR"
   "$TESTSRC_DIR"
+  "$SUPPORTSRC_DIR"
 )
 
 let errors=0
 for d in "${SRC_DIRS[@]}"; do
   echo -e "\n${_openGroup}Checking syntax of files in '$d'"
+  echo "### Node scripts ###"
   for srcFile in "$d"/**/*.{js,mjs}; do
     echo "$srcFile:"
     node -c "$srcFile"
+    let errors="$errors + $?"
+  done
+
+  echo -e "\n### Bash scripts (sh, lib) ###"
+  for srcFile in "$d"/**/*.{sh,lib}; do
+    echo "$srcFile:"
+    bash -n "$srcFile"
+    let errors="$errors + $?"
+  done
+
+  echo -e "\n### Bash scripts (no extension) ###"
+  IFS=$'\n' noExt=($(find "$d" -not -name '*.*'))
+  for srcFile in "${noExt[@]}"; do
+    ! [ -f "$srcFile" ] && continue
+
+    [ -z "$(file "$srcFile" | grep Bourne-Again)" ] && continue
+
+    echo "$srcFile:"
+    bash -n "$srcFile"
     let errors="$errors + $?"
   done
   echo "$_endGroup"

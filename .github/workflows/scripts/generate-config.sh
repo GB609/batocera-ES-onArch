5a1c41d65cd7791c9c8a151250cc062af35c049a
GB609 <39741460+GB609@users.noreply.github.com>
2025-07-21 17:18:09 +0000

    create script to generate configs
    still WIP

---
+++ .github/workflows/scripts/generate-config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+branchName=$(git rev-parse --abbrev-ref HEAD || exit 1)
+
+source ./PKGBUILD
+
+for sourceFile in "${source[@]}"; do
+  localPath="${}"
+  localName="$(basename "$localPath")"
+  [[ "$localName" =~ .*"."[a-z]{3,4}$ ]] || continue
+  
+  remoteUrl="${}"
+
+  mkdir -p "$(dirname "$localPath")"
+  curl "$remoteUrl" > $localPath
+done
+
+_generateConfig
+
+rm -rf ./configs-generated
+cp "$SRCDEST"/rootfs ./configs-generated
+git add ./configs-generated
+
+git commit -m 'auto-generate reference configuration'
+git push -f origin "$branchName":"configs/$branchName"

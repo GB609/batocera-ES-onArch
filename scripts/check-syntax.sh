74cf3e72060e84997ae5c64ee1026aece4c3c165
GB609 <39741460+GB609@users.noreply.github.com>
2025-08-21 08:35:45 +0200

    some additions to prevent strange shell errors
    also fixed a bug where such a dangling slash created a directory with a
    single blank as name

--- scripts/check-syntax.sh
+++ scripts/check-syntax.sh
@@ -18,33 +18,46 @@ SRC_DIRS=(
   "$SUPPORTSRC_DIR"
 )
 
+function checkBashFile {
+  local result=0
+  echo "$1":
+  bash -n "$1" || (( result+=1 ))
+
+  # Special checks for very troublesome and nearly invisible problems
+  # dangling \ for line-wrapping followed by any whitespace
+  output=$(grep --color=always -E '\\ +$' "$1")
+  if [ -n "$output" ]; then
+    (( result+=1 ))
+    xargs -d $'\n' printf '\e[31mDangling backslash:\e[0m [%s$]\n' << EOF 
+$output 
+EOF
+  fi
+  # && (( result+=1 ))
+
+  return "$result"
+}
+
 let errors=0
 for d in "${SRC_DIRS[@]}"; do
   echo -e "\n${_openGroup}Checking syntax of files in '$d'"
   echo "### Node scripts ###"
   for srcFile in "$d"/**/*.{js,mjs}; do
     echo "$srcFile:"
-    node -c "$srcFile"
-    let errors="$errors + $?"
+    node -c "$srcFile" || (( errors+=1 ))
   done
 
   echo -e "\n### Bash scripts (sh, lib) ###"
   for srcFile in "$d"/**/*.{sh,lib}; do
-    echo "$srcFile:"
-    bash -n "$srcFile"
-    let errors="$errors + $?"
+    checkBashFile "$srcFile" || (( errors+=1 ))
   done
 
   echo -e "\n### Bash scripts (no extension) ###"
   IFS=$'\n' noExt=($(find "$d" -not -name '*.*'))
   for srcFile in "${noExt[@]}"; do
     ! [ -f "$srcFile" ] && continue
-
     [ -z "$(file "$srcFile" | grep Bourne-Again)" ] && continue
 
-    echo "$srcFile:"
-    bash -n "$srcFile"
-    let errors="$errors + $?"
+    checkBashFile "$srcFile" || (( errors+=1 ))
   done
   echo "$_endGroup"
 done

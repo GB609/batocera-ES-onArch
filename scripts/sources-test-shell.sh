cf1096fea2bd1ea869111c07008eaa8e8dbbd9c3
GB609 <39741460+GB609@users.noreply.github.com>
2025-10-04 02:30:08 +0200

    fix up configuration of test shell script

--- scripts/sources-test-shell.sh
+++ scripts/sources-test-shell.sh
@@ -6,32 +6,41 @@
 
 # First unset all in the current shell to make sure configurations of the real system dont interfere
 (
+  ROOT_DIR="$(dirname "$(readlink -f "$0")")"
+  ROOT_DIR="$(realpath -s "$ROOT_DIR/..")"
+  TEST_ROOT="$ROOT_DIR"/tmp/FS_ROOT
+  mkdir -p "$TEST_ROOT"
+
+  "$ROOT_DIR"/scripts/generate-config.sh "$TEST_ROOT"
+  ln -s "$ROOT_DIR"/pkg/batocera-emulationstation/opt/batocera-emulationstation/bin/resources \
+        "$TEST_ROOT"/opt/batocera-emulationstation/bin/resources
+  ln -s "$ROOT_DIR"/pkg/batocera-emulationstation/opt/batocera-emulationstation/bin/emulationstation \
+        "$TEST_ROOT"/opt/batocera-emulationstation/bin/emulationstation
+  rsync -a "$ROOT_DIR"/test/resource/fs_root/home/ "$TEST_ROOT"/home/
+  
   # ... But do everything in a sub-shell to not destroy the real environment
   unset XDG_CONFIG_HOME XDG_DATA_HOME XDG_STATE_HOME XDG_CACHE_HOME
   unset ES_HOME ES_CONFIG_HOME ES_DATA_DIR ES_STATE_DIR ES_CACHE_DIR
   unset DEFAULT_ROMS_ROOT ROMS_ROOT_DIR SAVES_ROOT_DIR
   unset EMU_CMD_DIR EMU_CFG_DIR
 
-  ROOT_DIR="$(dirname "$(readlink -f "$0")")"
-  ROOT_DIR="$(realpath -s "$ROOT_DIR/..")"
-
   # for all using common-paths.lib
   export FS_ROOT="$ROOT_DIR"/sources/fs-root
-  export HOME="$ROOT_DIR"/test/resource/fs_root/home/test
-  export ES_CONFIG_HOME="$ROOT_DIR"/test/resource/fs_root/home/test/.emulationstation
-  export CONFIG_ROOT="$ROOT_DIR"/test/resource/fs_root/configs
-  export ROMS_ROOT_DIR="$ROOT_DIR"/test/resource/fs_root/home/test/ROMs
+  export HOME="$TEST_ROOT"/home/test
+  export ES_CONFIG_HOME="$HOME"/.emulationstation
+  export CONFIG_ROOT="$TEST_ROOT"/etc/batocera-emulationstation
+  export ROMS_ROOT_DIR="$HOME"/ROMs
 
   # usr/bin/emulatorlauncher, normally under CONFIG_ROOT
   #export EMU_CFG_DIR=""
 
   # btc-config
-  export DROPIN_PATH="$ROOT_DIR"/test/resource/fs_root/configs/dropins
+  export DROPIN_PATH="$CONFIG_ROOT"/conf.d
 
   # btc-config and usr/bin/emulationstation
   #this only works if the package has been built at least once
-  export BTC_BIN_DIR="$ROOT_DIR"/test/resource/fs_root/btcDir/bin
-  
+  export BTC_BIN_DIR="$TEST_ROOT"/opt/batocera-emulationstation/bin
+
   export PATH="$FS_ROOT/usr/bin:$PATH"
   mkdir -p "$HOME" "$CONFIG_ROOT" "$DROPIN_PATH"
 

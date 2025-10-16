befc206d3d5e989cfe67037b67c5b0ef24fb4379
GB609 <39741460+GB609@users.noreply.github.com>
2025-10-16 22:15:07 +0200

    [test] add a test for common-paths.lib

---
+++ test/shell/opt/batocera-emulationstation/common-paths.lib.test.js
@@ -0,0 +1,59 @@
+Object.assign(globalThis, require('test-helpers.mjs'));
+const assert = require('node:assert/strict');
+
+enableLogfile();
+
+const FILE_UNDER_TEST = 'opt/batocera-emulationstation/common-paths.lib';
+
+class CommonPathsTest extends ShellTestRunner {
+
+  beforeEach(ctx) {
+    super.beforeEach(ctx);
+    this.testFile(FILE_UNDER_TEST);
+    this.environment({ HOME: process.env.ES_HOME });
+    //use verify to mock away mkdir
+    this.verifyFunction('mkdir');
+  }
+
+  verifyDefaultConfigRoot() {
+    this.verifyVariable("CONFIG_ROOT", "/etc");
+    this.execute();
+  }
+
+  verifyConfigRootFromOutside() {
+    this.environment({ CONFIG_ROOT: TMP_DIR });
+    this.verifyVariable("CONFIG_ROOT", TMP_DIR);
+    this.execute();
+  }
+
+  verifyXdgDirs() {
+    this.environment({
+      HOME: TMP_DIR,
+      XDG_RUNTIME_DIR: '/dev/null'
+    });
+    this.verifyVariables({
+      XDG_RUNTIME_DIR: '/dev/null',
+      XDG_CONFIG_HOME: TMP_DIR + '/.config',
+      XDG_DATA_HOME: TMP_DIR + '/.local/share',
+      XDG_STATE_HOME: TMP_DIR + '/.local/state',
+      XDG_CACHE_HOME: TMP_DIR + '/.cache',
+    });
+    this.execute();
+  }
+  
+  esDirs(){
+    
+  }
+  userDirs(){}
+  
+  verifyExportedVariables(){
+    this.verifyExports(
+      "ES_HOME", "ES_CONFIG_HOME",
+      "ROMS_ROOT_DIR", "SAVES_ROOT_DIR"
+    )
+    
+    this.execute()
+  }
+}
+
+runTestClass(CommonPathsTest, FILE_UNDER_TEST)

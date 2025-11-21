43b38d053c288ac25e46bd1d52831cb163998cc0
GB609 <39741460+GB609@users.noreply.github.com>
2025-11-21 10:05:17 +0100

    add test for `logging.lib`

---
+++ test/shell/opt/batocera-emulationstation/logging.lib.test.js
@@ -0,0 +1,76 @@
+const fs = require('node:fs');
+const { ShellTestRunner } = require('js/utils/shelltest.mjs');
+
+enableLogfile();
+
+const FILE_UNDER_TEST = 'opt/batocera-emulationstation/logging.lib';
+
+class LoggingTest extends ShellTestRunner {
+  beforeEach(ctx) {
+    super.beforeEach(ctx);
+    this.testFile(FILE_UNDER_TEST);
+    this.preActions.push('exec 3>&2')
+    this.arguments(`${this.TMP_DIR}/shell.log`);
+    this.verifyFunction('exec');
+  }
+
+  failsWithoutLogArgument() {
+    delete this.functionVerifiers.exec;
+    this.arguments();
+    assert.throws(() => this.execute(), /Failed with no output!/);
+  }
+
+  _logOnly() {
+    this.verifyFunction('echo', '-e', "Hello, this has blanks", 'plus', 'something', false);
+    this.postActions('_logOnly "Hello, this has blanks" plus something false');
+    this.execute();
+  }
+
+  _logAndOut() {
+    this.verifyFunction('_logOnly', "Hello, this has blanks", 'plus', 'something', false);
+    this.verifyFunction('echo', '-e', "Hello, this has blanks", 'plus', 'something', false);
+    this.postActions(
+      this.functionVerifiers._logOnly,
+      '_logAndOut "Hello, this has blanks" plus something false'
+    );
+    this.execute();
+  }
+
+  _logAndOutWhenDebug_DISABLED() {
+    this.environment({ PRINT_DEBUG: '' });
+    this.verifyFunction('_logOnly', "Hello, this has blanks", 'plus', 'something', false);
+    this.postActions(
+      this.functionVerifiers._logOnly,
+      '_logAndOutWhenDebug "Hello, this has blanks" plus something false'
+    );
+    this.execute();
+  }
+
+  _logAndOutWhenDebug_ENABLED() {
+    this.environment({ PRINT_DEBUG: true });
+    this.verifyFunction('_logOnly', "Hello, this has blanks", 'plus', 'something', false);
+    this.verifyFunction('echo', '-e', "Hello, this has blanks", 'plus', 'something', false);
+    this.postActions(
+      this.functionVerifiers._logOnly,
+      '_logAndOutWhenDebug "Hello, this has blanks" plus something false'
+    );
+    this.execute();
+  }
+
+  _pipeDebugLog_DISABLED() {
+    this.environment({ PRINT_DEBUG: '' });
+    this.verifyFunction('cat');
+    this.postActions('echo "output test" | _pipeDebugLog');
+    this.execute();
+  }
+
+  _pipeDebugLog_ENABLED() {
+    this.environment({ PRINT_DEBUG: 'notEmpty' });
+    this.verifyFunction('tee', '-a', `${this.TMP_DIR}/shell.log`);
+    this.postActions('echo "output test" | _pipeDebugLog');
+    this.execute();
+  }
+}
+
+
+runTestClass(LoggingTest);

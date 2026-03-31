971ddef184f3a56beb4de50e0b2a9c6f5f5ab9d0
GB609 <39741460+GB609@users.noreply.github.com>
2026-03-31 14:16:53 +0200

    added test cases and fixed issues detected

--- test/js/libs/parsing.test.js
+++ test/js/libs/parsing.test.js
@@ -58,15 +58,6 @@ class ParserTests {
       //console.error(JSON.stringify(old.features.sharedFeatures.feature[0], null, 2))
     }
   */
-  newYaml() {
-    let file = `${ROOT_PATH}/tmp/es_features.yml`;
-    let newStyle = parser.yamlToDict_IMPL(file);
-
-    //file = `${ROOT_PATH}/tmp/es_systems.yml`;
-    //newStyle = parser.yamlToDict_IMPL(file);
-    //console.error(JSON.stringify(newStyle, null, 2));
-    assert.fail()
-  }
 
   parseLineCommentedJsonString() {
     let source = `{
@@ -218,10 +209,9 @@ colonAsContent: :start :end
   }
 
   comments() {
-    // FIXME/TODO: YAML spec completely disallows comments within multiline blocks,
-    // the internal parser SKIPS them for now 
     let data =
       `
+%ignore directives
 commentFirst: #ignored
   value
 commentAfter: [true] #ignore
@@ -243,6 +233,189 @@ list: #  list header
       commentAfter: [true],
       'ml-string': 'content start content end',
       list: ['value', 'v2']
+    }
+
+    let result = parser.yamlToDict_IMPL(data)
+    assert.deepEqual(sanitizeDataObject(result), expected);
+  }
+
+  dictNesting_basic() {
+    let data = `
+topLevel:
+  middle:
+    last: true
+  oneUp:
+    deeper: string
+fullUp: 88`;
+
+    let expected = {
+      topLevel: {
+        middle: { last: true },
+        oneUp: { deeper: 'string' }
+      },
+      fullUp: 88
+
+    }
+
+    let result = parser.yamlToDict_IMPL(data)
+    assert.deepEqual(sanitizeDataObject(result), expected);
+  }
+
+  listNesting_basic() {
+    let data = `
+- A
+- B
+- no
+- yes`;
+
+    let expected = ['A', 'B', 'no', 'yes']
+
+    let result = parser.yamlToDict_IMPL(data)
+    assert.deepEqual(sanitizeDataObject(result), expected);
+  }
+
+  nesting_mixed() {
+    let data = `
+topLevel:
+  listSameIndent:
+  - entry 1
+  - subDict:
+      deeper: 42
+  listSibling: true
+  deeperList:
+    - 
+      key1: ofObjInList1  
+      key2: ofObjInList2
+    - false
+allClosed: true
+multiDimensional:
+- - A
+  - B
+- - 1
+  - 2`;
+
+    let expected = {
+      topLevel: {
+        listSameIndent: [
+          'entry 1',
+          { subDict: { deeper: 42 } }
+        ],
+        listSibling: true,
+        deeperList: [
+          {
+            key1: 'ofObjInList1',
+            key2: 'ofObjInList2',
+          },
+          false
+        ]
+      },
+      allClosed: true,
+      multiDimensional: [
+        ['A', 'B'],
+        [1, 2]
+      ]
+    }
+
+    let result = parser.yamlToDict_IMPL(data)
+    assert.deepEqual(sanitizeDataObject(result), expected);
+  }
+
+  flowStructure() {
+    let data = `
+dict: { one: 1, two: 2,
+  three: 3 }
+arr: 
+  [ 5, 6, 7, 8
+  ]
+d>a: {
+  subArr: [ true, false, true ]
+}
+a>d: [ {c: A}, {c: 0.2} ]
+`;
+
+    let expected = {
+      dict: { one: 1, two: 2, three: 3 },
+      arr: [5, 6, 7, 8],
+      'd>a': { subArr: [true, false, true] },
+      'a>d': [{ c: 'A' }, { c: 0.2 }]
+    }
+
+    let result = parser.yamlToDict_IMPL(data)
+    assert.deepEqual(sanitizeDataObject(result), expected);
+  }
+
+  flowStrings() {
+    let data = `
+mixedFlow: [
+  45.6,
+  broken down
+  multiline flow
+  string,
+  false
+  ]
+dq:
+     "  starts with 2 blanks, then\\
+            <previous (masked) should be IGNORED
+        <previous should be folded\\"
+        \\n<-valid LF escape"
+sq: ' start with BLNK ''<-masked
+    \\n<-escapes not evaluated'
+`;
+
+    let expected = {
+      mixedFlow: [45.6, 'broken down multiline flow string', false],
+      dq: '  starts with 2 blanks, then<previous (masked) should be IGNORED <previous should be folded" \n<-valid LF escape',
+      sq: " start with BLNK '<-masked \\n<-escapes not evaluated"
+    }
+
+    let result = parser.yamlToDict_IMPL(data)
+    assert.deepEqual(sanitizeDataObject(result), expected);
+  }
+
+  multilineStrings() {
+    let data = `
+mixedFlow: [
+  45.6,
+  broken down
+  multiline flow
+  string,
+  false
+  ]
+literalBlock: | # also tests default chomp mode 'clip'
+  line with 2 trailing spaces  
+
+  line after EMPTY line
+
+  
+  
+foldedBlock: >
+  1 trailing space 
+
+  <after EMPTY
+  <folded to space
+  
+strip: |-
+  don't keep trailing LF
+
+keep: |+
+  4 newlines
+  
+  
+  
+
+indent2: >2-
+    deeper text
+    in 2 lines
+  indicated level
+`
+    let expected = {
+      mixedFlow: [45.6, 'broken down multiline flow string', false],
+      literalBlock: 'line with 2 trailing spaces  \n\nline after EMPTY line\n',
+      foldedBlock: '1 trailing space \n<after EMPTY <folded to space\n',
+      strip: "don't keep trailing LF",
+      keep: '4 newlines\n\n\n\n',
+      //line breaks on transitions between indented/none-indented are kept
+      indent2: '  deeper text\n  in 2 lines\nindicated level'
     }
 
     let result = parser.yamlToDict_IMPL(data)

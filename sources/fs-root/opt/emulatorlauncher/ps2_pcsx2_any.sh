79876864ba353151e71bb0b842ea851ccb8180f7
GB609 <39741460+GB609@users.noreply.github.com>
2026-06-16 09:28:13 +0000

    Update ps2_pcsx2_any.sh

--- sources/fs-root/opt/emulatorlauncher/ps2_pcsx2_any.sh
+++ sources/fs-root/opt/emulatorlauncher/ps2_pcsx2_any.sh
@@ -2,5 +2,11 @@
 #
 # SPDX-License-Identifier: MIT
 
-local executable=""
-launchCommand=("$executable" -batch -fullscreen -fastboot -- "$rom")
+executable="$(locateExecutable "pcsx2")"
+launchCommand=("$executable" -nogui -fullscreen -fastboot -datapath "${CONFIG_FILE_PATH}" -- "$rom")
+
+if ! [ -d "${CONFIG_FILE_PATH}" ]; then
+  mkdir -p "${CONFIG_FILE_PATH}"
+fi
+
+#TODO: write property files to "${CONFIG_FILE_PATH}", and link save dir (when there is no direct config option to specify save path)
